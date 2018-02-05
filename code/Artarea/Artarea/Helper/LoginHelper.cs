efaa01eb81abfa57920f0b85dda531a61cd222cd
datvit <davit.abjanadze05@gmail.com>
2018-02-05 13:21:58 +0400

    mvc project
    code

---
+++ code/Artarea/Artarea/Helper/LoginHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Artarea.Helper
+{
+    public class LoginHelper
+    {
+        public static string SmtpServer = "";
+        public static string UserName = "";
+    }
+}
---
+++ code/Artarea/Artarea/Helper/PasswordHashHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Artarea.Helper
+{
+    public class PasswordHashHelper
+    {
+        public static string Password = "KcvCIpmllHwZODYaIutulw";
+    }
+}
