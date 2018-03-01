77281295e334441439fa16d5375f14a82bd4c8d4
datvit <davit.abjanadze05@gmail.com>
2018-03-01 11:50:09 +0400

    ფრონტის მიბმა ბექზე

---
+++ Project/ArtareaNew/ArtareaNew/Controllers/ProgramController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArtareaNew.Controllers
+{
+    public class ProgramController : Controller
+    {
+        // GET: Program
+        public ActionResult Programs()
+        {
+            return View();
+        }
+
+        public ActionResult VisualArts()
+        {
+            return View();
+        }
+
+        public ActionResult Series()
+        {
+            return View();
+        }
+    }
+}
